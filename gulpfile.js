ea5a501c0de8ce6490bf11bc9dccd21c5c468a11
dpilafian <dem@centerkey.com>
2023-07-08 02:20:57 -0700

    Disable deprecated features in marked

--- gulpfile.js
+++ gulpfile.js
@@ -62,7 +62,7 @@ const getPageData = () => {
    const includes = config.includes
       .map(include => `source/includes/${include}.md`)
       .map(include => fs.readFileSync(include, 'utf-8'))
-      .map(include => marked(include, { renderer: renderer }));
+      .map(include => marked(include, { renderer: renderer, mangle: false, headerIds: false }));
    const code = (filename) => filename.split('.')[0];
    const getPageData = {
       current_page: { data: config },
